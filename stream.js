5e26d8bcd3b3e0a3bef8739cc0f5e5a29f241527
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-03 10:47:55 +0300

    Replace event listener on createReadStream with a pipe

--- stream.js
+++ stream.js
@@ -4,6 +4,8 @@ const rs = fs.createReadStream('./files/lorem.txt', {encoding: 'utf-8'})
 
 const ws = fs.createWriteStream('./files/new-lorem.txt')
 
-rs.on('data', (dataChunk) => {
-  ws.write(dataChunk)
-})
+// rs.on('data', (dataChunk) => {
+//   ws.write(dataChunk)
+// })
+
+rs.pipe(ws)
