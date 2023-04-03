24fbdad6a64a87665ea2912391a2b4131d7d3c42
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-03 08:53:53 +0300

    Add writeFile method, Replace hardcoded path with __dirname import from path

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const fs = require('fs')
+const path = require('path')
 
-fs.readFile('./files/starter.txt', 'utf-8', (err, data) => {
+fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8', (err, data) => {
   if(err) throw err
   console.log(data)
 })
 
 //Will be displayed first
 console.log('Hello...')
+
+fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you', (err) => {
+  if(err) throw err
+  console.log('Write operation complete')
+})
 
 //Exit on Uncaught errors
 process.on('uncaughtException', err => {
