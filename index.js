351c57f21259c81e120591d5e9131d7a5581962e
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-03 08:32:26 +0300

    Replace toString() with utf-8. Add Exit on Uncaught errors

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const fs = require('fs')
 
-fs.readFile('./files/starter.txt', (err, data) => {
+fs.readFile('./files/starter.txt', 'utf-8', (err, data) => {
   if(err) throw err
-  console.log(data.toString())
+  console.log(data)
 })
 
+//Will be displayed first
+console.log('Hello...')
 
+//Exit on Uncaught errors
+process.on('uncaughtException', err => {
+  console.error(`There was uncaught error: ${err}`)
+  process.exit(1)
+})
