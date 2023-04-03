853def15f2d2768eb5726726b73ce55c9dd0d120
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-03 10:34:20 +0300

    Add Stream to read and write large chunks of data

---
+++ stream.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+
+const rs = fs.createReadStream('./files/lorem.txt', {encoding: 'utf-8'})
+
+const ws = fs.createWriteStream('./files/new-lorem.txt')
+
+rs.on('data', (dataChunk) => {
+  ws.write(dataChunk)
+})
