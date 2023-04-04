26f8cc99782452b0e3e2a3ab1985278af5a53199
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-04 08:20:55 +0300

    Add html pages and the img folder

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3500
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method)
+  
 })
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
