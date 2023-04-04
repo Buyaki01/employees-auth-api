114d6bcda02455f2b940c5e4653777867935fadb
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-04 07:59:31 +0300

    Add PORT

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+const fsPromises = require('fs').promises
+
 const logEvents = require('./logEvents')
 const EventEmitter = require('events')
 class  Emitter extends EventEmitter{}
@@ -5,4 +10,10 @@ const myEmitter = new Emitter()
 // myEmitter.on('log', (msg) => logEvents(msg))
 // myEmitter.emit('log', 'Log event emitted!')
 
+const PORT = process.env.PORT || 3500 
 
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method)
+})
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
