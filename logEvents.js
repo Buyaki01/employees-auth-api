29a9dc46d489591d8b6e7e9bd033f3f8305cb61b
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-04 07:21:00 +0300

    Add function where if dir does not exist it should be created, create object for MyEmitter, add listener to the log event

--- logEvents.js
+++ logEvents.js
@@ -7,12 +7,15 @@ const path = require('path')
 
 const logEvents = async (message) => {
   const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`
-  const logItem = `${dateTime}\t{uuid()\t${message}}`
+  const logItem = `${dateTime}\t{uuid()\t${message}}\n`
   console.log(logItem)
   try{
+    if(!fs.existsSync(path.join(__dirname, 'logs'))){
+      await fsPromises.mkdir(path.join(__dirname, 'logs'))
+    }
     await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem)
   }catch(err){
-    console.err(err)
+    console.error(err)
   }
 }
 
