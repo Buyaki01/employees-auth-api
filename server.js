4c0d5ac7b9949eb8565eddb6e280a6ab6b7e4a79
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-04 09:12:35 +0300

    Add filepath for different extensions

--- server.js
+++ server.js
@@ -41,6 +41,19 @@ const server = http.createServer((req, res) => {
       contentType = 'text/html';
   }
 
+  let filePath =
+    contentType === 'text/html' && req.url === '/'
+      ? path.join(__dirname, 'views', 'index.html')
+      : contentType === 'text/html' && req.url.slice(-1) === '/'
+          ? path.join(__dirname, 'views', req.url, 'index.html')
+          : contentType === 'text/html'
+              ? path.join(__dirname, 'views', req.url)
+              : path.join(__dirname, req.url);
+    
+    //This is in the case where user requests for a page without writing .html on the browser. Makes provision where extensions are provided.
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+    
 })
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
