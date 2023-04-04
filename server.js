fb9fc104c8d9b23d3d7ef99c080ea37fc8516418
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-04 08:42:07 +0300

    Add different cases of extension

--- server.js
+++ server.js
@@ -14,7 +14,33 @@ const PORT = process.env.PORT || 3500
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method)
-  
+
+  const extension = path.extname(req.url)
+  let contentType
+
+  switch (extension) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.jpg':
+      contentType = 'image/jpeg';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.txt':
+      contentType = 'text/plain';
+      break;
+    default:
+      contentType = 'text/html';
+  }
+
 })
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
