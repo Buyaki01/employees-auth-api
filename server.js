f11e348f206608888a7565de93cde0cd6763afb7
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-05 07:49:13 +0300

    Add content to data/data.json and data/data.txt. And add more content in cases of application/json and image

--- server.js
+++ server.js
@@ -7,18 +7,28 @@ const logEvents = require('./logEvents')
 const EventEmitter = require('events')
 class  Emitter extends EventEmitter{}
 const myEmitter = new Emitter()
-// myEmitter.on('log', (msg) => logEvents(msg))
-// myEmitter.emit('log', 'Log event emitted!')
+
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName))
 
 const PORT = process.env.PORT || 3500 
 
 const serveFile = async (filePath, contentType, response) => {
   try{
-    const data = await fsPromises.readFile(filePath, 'utf-8')
-    response.writeHead(200, {'Content-Type': contentType})
-    response.end(data)
+    const rawData = await fsPromises.readFile(
+      filePath, 
+      !contentType.includes('image') ? 'utf-8' : ''
+    )
+    const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData
+    response.writeHead(
+      filePath.includes('404.html') ? 404 : 200, 
+      {'Content-Type': contentType}
+    )
+    response.end(
+      contentType === 'application/json' ? JSON.stringify(data) : data
+    )
   }catch(err){
     console.log(err)
+    myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt')
     response.statusCode = 500
     response.end()
   }
@@ -26,6 +36,8 @@ const serveFile = async (filePath, contentType, response) => {
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method)
+
+  myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt')
 
   const extension = path.extname(req.url)
   let contentType
