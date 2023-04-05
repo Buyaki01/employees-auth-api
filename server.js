6b52c2ded155cdbe16acbb012b3cddc146be2dc9
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-05 08:35:00 +0300

    Add path for GET new-page.html

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const path = require('path')
 const PORT = process.env.PORT || 3500 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  // res.sendFile('./views/index.html', { root: __dirname })
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/new-page.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
