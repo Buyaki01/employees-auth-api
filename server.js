8bbcfcb14ad4c917137ac9e2b9d0e8440cafa84e
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-05 08:57:28 +0300

    Add redirect path

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ app.get('^/$|/index(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
-app.get('/new-page.html', (req, res) => {
+app.get('/new-page(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
+})
+
+app.get('/old-page(.html)?', (req, res) => {
+  res.redirect(301, '/new-page.html') //302 by default
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
