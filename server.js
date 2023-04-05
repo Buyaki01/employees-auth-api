60811c16a3dfb148f78cb51d0170fe45ceb28e96
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-05 08:21:09 +0300

    Send Hello World to each GET request

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500 
 
-
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
