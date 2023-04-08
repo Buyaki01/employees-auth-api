ae11eacd1b9a40f9d5edfc3f0dfadbadfd0b0d0c
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-08 11:48:44 +0300

    Add corsOtions as per cors documentation in npmjs.com

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.use(logger)
 
 //Cross Origin Resourse Sharing
 const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if(whitelist.indexOf(origin !== -1)){
+      callback(null, true)
+    }else{
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 app.use(cors())
 
 //Middleware to handle form data
