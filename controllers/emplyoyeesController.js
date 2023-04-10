9299b6de1c49bcbbd16e6624d3f5214826c29605
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-10 12:44:10 +0300

    Rename data folder to model, Add controllers folder

---
+++ controllers/emplyoyeesController.js
@@ -0,0 +1,32 @@
+const data = {}
+data.employees = require('../model/employees.json')
+
+const getAllEmployees = (req, res) => {
+  res.json(data.employees)
+}
+
+const createNewEmployees = (req, res) => {
+  res.json({
+    "firstname": req.body.firstname,
+    "lastname": req.body.lastname
+  })
+}
+
+const updateEmployee = (req, res) => {
+  res.json({
+    "firstname": req.body.firstname,
+    "lastname": req.body.lastname
+  })
+}
+
+const deleteEmployee = (req, res) => {
+  res.json({ "id": req.body.id })
+}
+
+const getEmployee = (req, res) => {
+  res.json({"id": req.params.id})
+}
+
+module.exports = {
+  getAllEmployees, createNewEmployees, updateEmployee, deleteEmployee, getEmployee
+}
--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,32 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
-const data = {}
-data.employees = require('../../data/employees.json')
 
 router.route('/')
-  .get((req, res) => {
-    res.json(data.employees)
-  })
-  .post((req, res) => {
-    res.json({
-      "firstname": req.body.firstname,
-      "lastname": req.body.lastname
-    })
-  })
-  .put((req, res) => {
-    res.json({
-      "firstname": req.body.firstname,
-      "lastname": req.body.lastname
-    })
-  })
-  .delete((req, res) => {
-    res.json({ "id": req.body.id })
-  })
+  .get()
+  .post()
+  .put()
+  .delete()
 
   router.route('/:id')
-    .get((req, res) => {
-      res.json({"id": req.params.id})
-    })
+    .get()
 
 module.exports = router
