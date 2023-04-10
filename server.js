d931473bcb7693826e0df706bfd0e2d88847c7ea
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-10 08:18:32 +0300

    Add more functionality to the 404 method

--- server.js
+++ server.js
@@ -70,7 +70,14 @@ const three = (req, res, next) => {
 app.get('/chain(.html)?', [one, two, three])
 
 app.all('*', (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404)
+  if (req.accepts('html')){
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  }else if(req.accepts('json')){
+    res.json({ error: "404 Not Found" })
+  }else{
+    res.type('txt').send("404 Not Found")
+  }
 })
 
 app.use(errorHandler)
