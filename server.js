1201deb3ddb6271816ac3ea505f4845d7b48ffee
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-10 07:13:39 +0300

    Enable CORS to allow undefined

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(logger)
 const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
 const corsOptions = {
   origin: (origin, callback) => {
-    if(whitelist.indexOf(origin !== -1)){
+    if(whitelist.indexOf(origin !== -1 || !origin)){
       callback(null, true)
     }else{
       callback(new Error('Not allowed by CORS'))
