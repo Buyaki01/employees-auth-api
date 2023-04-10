43154b31ba7458aa29031a31bc9d4c87e6bbcd1e
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-10 10:11:21 +0300

    Add routes for the files in the root

--- server.js
+++ server.js
@@ -33,20 +33,8 @@ app.use(express.json())
 app.use('/', express.static(path.join(__dirname, '/public')))
 app.use('/subdir', express.static(path.join(__dirname, '/public')))
 
+app.use('/', require('./routes/root'))
 app.use('/subdir', require('./routes/subdir'))
-
-app.get('^/$|/index(.html)?', (req, res) => {
-  // res.sendFile('./views/index.html', { root: __dirname })
-  res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
-
-app.get('/new-page(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
-})
-
-app.get('/old-page(.html)?', (req, res) => {
-  res.redirect(301, '/new-page.html') //302 by default
-})
 
 //Route Handlers
 app.get('/hello(.html)?', (req, res, next) => {
@@ -55,24 +43,6 @@ app.get('/hello(.html)?', (req, res, next) => {
 }, (req, res) => {
   res.send('Hello World!')
 })
-
-//Chaining Route Handler
-const one = (req, res, next) => {
-  console.log('one')
-  next()
-}
-
-const two = (req, res, next) => {
-  console.log('two')
-  next()
-}
-
-const three = (req, res, next) => {
-  console.log('three')
-  res.send('Finished')
-}
-
-app.get('/chain(.html)?', [one, two, three])
 
 app.all('*', (req, res) => {
   res.status(404)
