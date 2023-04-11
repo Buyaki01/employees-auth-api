5bf015d706a8c8600a700a2ecee4dbff69db0f94
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-11 15:16:00 +0300

    Add handleNewUser method

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -7,3 +7,16 @@ const fsPromises = require('fs').promises
 const path = require('path')
 const bcrypt = require('bcrypt')
 
+const handleNewUser = async (req, res) => {
+  const { user, pwd } = req.body
+  if(!user || !pwd) return res.status(400).json({ 'message': 'Username and Password are required.' })
+  // Check for duplicate usernames in the database
+  const duplicate = usersDB.users.find(person => person.username === user)
+  if(duplicate) return res.sendStatus(409) //Conflict
+  try{
+    //encrypt the password
+    
+  }catch(err){
+    res.status(500).json({ 'message': err.message })
+  }
+}
