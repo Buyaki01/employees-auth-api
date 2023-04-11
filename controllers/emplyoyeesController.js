b0faef2a9775b69e623eac2a835db8aa19085785
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-11 09:55:57 +0300

    Update deleteEmployee method

--- controllers/emplyoyeesController.js
+++ controllers/emplyoyeesController.js
@@ -9,7 +9,7 @@ const getAllEmployees = (req, res) => {
 
 const createNewEmployees = (req, res) => {
   const newEmployee = {
-    id: data.employees?.length ? data.employees[data.employees.length - 1].id + 1 : 1,
+    id: data.employees[data.employees.length - 1].id + 1 || 1,
     firstname: req.body.firstname,
     lastname: req.body.lastname
   }
@@ -36,7 +36,13 @@ const updateEmployee = (req, res) => {
 }
 
 const deleteEmployee = (req, res) => {
-  res.json({ "id": req.body.id })
+  const employee = data.employees.find(emp => emp.id === parseInt(req.body.id))
+  if(!employee){
+    return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` })
+  }
+  const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id))
+  data.setEmployees([...filteredArray])
+  res.json(data.employees)
 }
 
 const getEmployee = (req, res) => {
