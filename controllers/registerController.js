1bc8321a8f5ddfad40493f33ec15a4a8338c4346
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-11 14:53:41 +0300

    Install bcrypt

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,4 +1,9 @@
 const usersDB = {
   users: require('../model/users.json'),
   setUsers: function(data) {this.users = data}
-} 
+}
+
+const fsPromises = require('fs').promises
+const path = require('path')
+const bcrypt = require('bcrypt')
+
