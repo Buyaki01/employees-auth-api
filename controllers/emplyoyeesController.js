1f5ef5f4a7e943aa94ce6df7d170823c45b8f6d2
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-11 07:29:48 +0300

    Update updateEmployee method

--- controllers/emplyoyeesController.js
+++ controllers/emplyoyeesController.js
@@ -9,7 +9,7 @@ const getAllEmployees = (req, res) => {
 
 const createNewEmployees = (req, res) => {
   const newEmployee = {
-    id: data.employees[data.employees.length-1].id + 1 || 1,
+    id: data.employees?.length ? data.employees[data.employees.length - 1].id + 1 : 1,
     firstname: req.body.firstname,
     lastname: req.body.lastname
   }
@@ -19,14 +19,20 @@ const createNewEmployees = (req, res) => {
   }
 
   data.setEmployees([...data.employees, newEmployee])
-  res.json(data.employees)
+  res.status(201).json(data.employees)
 }
 
 const updateEmployee = (req, res) => {
-  res.json({
-    "firstname": req.body.firstname,
-    "lastname": req.body.lastname
-  })
+  const employee = data.employees.find(emp => emp.id === parseInt(req.body.id))
+  if (!employee){
+    return res.status(400).json({ "message": `Employee ID ${req.body.id} not found`})
+  }
+  if(req.body.firstname) employee.firstname = req.body.firstname
+  if(req.body.lastname) employee.lastname = req.body.lastname
+  const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id))
+  const unsortedArray = [...filteredArray, employee]
+  data.setEmployees(unsortedArray.sort((a,b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
+  res.json(data.employees)
 }
 
 const deleteEmployee = (req, res) => {
