f0b8e0e9babcf5f77dcd9dfc456404dfc3f83c34
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-12 09:26:47 +0300

    Add store new user method

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -17,7 +17,17 @@ const handleNewUser = async (req, res) => {
     //encrypt the password
     const hashedPwd = await bcrypt.hash(pwd, 10)
     // store the new user
+    const newUser = { "username": user, "password": hashedPwd }
+    usersDB.setUsers([...usersDB.users, newUser])
+    await fsPromises.writeFile(
+      path.join(__dirname, '..', 'model', 'users.json'),
+      JSON.stringify(usersDB.users)  
+    )
+    console.log(usersDB.users)
+    res.status(201).json({ 'success': `New user ${user} created!`})
   }catch(err){
     res.status(500).json({ 'message': err.message })
   }
 }
+
+module.exports = { handleNewUser }
