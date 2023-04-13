da3fa7cd8094b494252e5af0a623c6228af1c957
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-13 11:31:42 +0300

    Add verifyJWT method to only the get route for employees

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const employeesController = require('../../controllers/emplyoyeesController')
+const verifyJWT = require('../../middleware/verifyJWT')
 
 router.route('/')
-  .get(employeesController.getAllEmployees)
+  .get(verifyJWT, employeesController.getAllEmployees) //If we just want it in the get
   .post(employeesController.createNewEmployees)
   .put(employeesController.updateEmployee)
   .delete(employeesController.deleteEmployee)
