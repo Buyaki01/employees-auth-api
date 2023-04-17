9aded537efbcb4b12ea6234418190f771ab4423a
Buyaki01 <rittahbuyaki@gmail.com>
2023-04-17 14:39:38 +0300

    Add roles in the user payload in the authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,9 +17,16 @@ const handleLogin = async (req, res) => {
   //Evaluate password
   const match = await bcrypt.compare(pwd, foundUser.password)
   if (match) {
+    const roles = Object.values(foundUser.roles)
     //Create JWTs
     const accessToken = jwt.sign(
-      { "username": foundUser.username},
+      //user payload
+      { 
+        UserInfo: {
+          "username": foundUser.username,
+          "roles": roles
+        }
+      },
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: '30s' }
     )
