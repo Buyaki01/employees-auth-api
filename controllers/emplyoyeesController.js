0f667dbd5af314b7353266f4809dff59b4205bd8
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-20 12:30:48 +0300

    Refactor employeesController with mongoDB

--- controllers/emplyoyeesController.js
+++ controllers/emplyoyeesController.js
@@ -1,58 +1,68 @@
-const data = {
-  employees: require('../model/employees.json'),
-  setEmployees: function (data) { this.employees = data }
+const Employee = require('../model/Employee');
+
+const getAllEmployees = async (req, res) => {
+  const employees = await Employee.find();
+  if (!employees) return res.status(204).json({ 'message': 'No employees found.' });
+  res.json(employees);
 }
 
-const getAllEmployees = (req, res) => {
-  res.json(data.employees)
+const createNewEmployee = async (req, res) => {
+  if (!req?.body?.firstname || !req?.body?.lastname) {
+    return res.status(400).json({ 'message': 'First and last names are required' });
+  }
+
+  try {
+    const result = await Employee.create({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname
+    });
+
+    res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-const createNewEmployees = (req, res) => {
-  const newEmployee = {
-    id: data.employees[data.employees.length - 1].id + 1 || 1,
-    firstname: req.body.firstname,
-    lastname: req.body.lastname
+const updateEmployee = async (req, res) => {
+  if (!req?.body?.id) {
+    return res.status(400).json({ 'message': 'ID parameter is required.' });
   }
 
-  if(!newEmployee.firstname || !newEmployee.lastname){
-    return res.status(400).json({ 'message': 'First and last names are required.' })
+  const employee = await Employee.findOne({ _id: req.body.id }).exec();
+  if (!employee) {
+    return res.status(204).json({ "message": `No employee matches ID ${req.body.id}.` });
   }
-
-  data.setEmployees([...data.employees, newEmployee])
-  res.status(201).json(data.employees)
+  if (req.body?.firstname) employee.firstname = req.body.firstname;
+  if (req.body?.lastname) employee.lastname = req.body.lastname;
+  const result = await employee.save();
+  res.json(result);
 }
 
-const updateEmployee = (req, res) => {
-  const employee = data.employees.find(emp => emp.id === parseInt(req.body.id))
-  if (!employee){
-    return res.status(400).json({ "message": `Employee ID ${req.body.id} not found`})
+const deleteEmployee = async (req, res) => {
+  if (!req?.body?.id) return res.status(400).json({ 'message': 'Employee ID required.' });
+
+  const employee = await Employee.findOne({ _id: req.body.id }).exec();
+  if (!employee) {
+    return res.status(204).json({ "message": `No employee matches ID ${req.body.id}.` });
   }
-  if(req.body.firstname) employee.firstname = req.body.firstname
-  if(req.body.lastname) employee.lastname = req.body.lastname
-  const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id))
-  const unsortedArray = [...filteredArray, employee]
-  data.setEmployees(unsortedArray.sort((a,b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
-  res.json(data.employees)
+  const result = await employee.deleteOne(); //{ _id: req.body.id }
+  res.json(result);
 }
 
-const deleteEmployee = (req, res) => {
-  const employee = data.employees.find(emp => emp.id === parseInt(req.body.id))
-  if(!employee){
-    return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` })
-  }
-  const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id))
-  data.setEmployees([...filteredArray])
-  res.json(data.employees)
-}
+const getEmployee = async (req, res) => {
+  if (!req?.params?.id) return res.status(400).json({ 'message': 'Employee ID required.' });
 
-const getEmployee = (req, res) => {
-  const employee = data.employees.find(emp => emp.id === parseInt(req.params.id))
-  if (!employee){
-    return res.status(400).json({ "message": `Employee ID ${req.params.id} not found`})
+  const employee = await Employee.findOne({ _id: req.params.id }).exec();
+  if (!employee) {
+    return res.status(204).json({ "message": `No employee matches ID ${req.params.id}.` });
   }
-  res.json(employee)
+  res.json(employee);
 }
 
 module.exports = {
-  getAllEmployees, createNewEmployees, updateEmployee, deleteEmployee, getEmployee
+  getAllEmployees,
+  createNewEmployee,
+  updateEmployee,
+  deleteEmployee,
+  getEmployee
 }
