ba9f6b3fc48d5a99395a459943b91ab528cfc99b
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-27 13:04:16 +0300

    Pass roles in the response

--- controllers/authController.js
+++ controllers/authController.js
@@ -65,7 +65,7 @@ const handleLogin = async (req, res) => {
       res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 })
 
       // Send authorization roles and access token to user
-      res.json({ accessToken })
+      res.json({ roles, accessToken })
 
   } else {
       res.sendStatus(401)
